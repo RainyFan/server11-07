119b1b194701142f4a5bc7d5b451344e09730439
Rainy <fany824@newschool.com>
2022-11-07 18:13:46 -0500

    updated

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 console.log("hi!")
+import express from 'express'
+const app = express()
+const port = process.env.PORT || 3001
+app.get('/', (req, res) => {
+    res.send("Draw a shishito pepper")
+  })
+  app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+  })
+  
